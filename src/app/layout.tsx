98f98e85df15c256e28327fce036ace251aeacf9
suttirakcha <suttirak.char@gmail.com>
2026-05-23 22:44:48 +0700

    feat: add thai font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
-import { Raleway, Send_Flowers } from "next/font/google";
+import { Noto_Sans_Thai, Raleway, Send_Flowers } from "next/font/google";
 import { Provider } from "@/components/ui/provider";
 import { Toaster } from "@/components/ui/toaster";
 import { NextIntlClientProvider } from "next-intl";
 import "./globals.css";
+import { cookies } from "next/headers";
 
 const sendFlowers = Send_Flowers({
   variable: "--font-send-flowers",
@@ -17,6 +18,12 @@ const raleway = Raleway({
   weight: ["300", "400", "500", "600", "700"],
 });
 
+const notoSansThai = Noto_Sans_Thai({
+  variable: "--font-noto-sans-thai",
+  subsets: ["thai"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+
 export const metadata: Metadata = {
   title: {
     template: "%s - Afternune",
@@ -25,14 +32,18 @@ export const metadata: Metadata = {
   description: "Let's have a chat together",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookieStore = await cookies();
+  const locale = cookieStore.get("locale")?.value ?? "en";
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${sendFlowers.variable} ${raleway.variable}`}>
+    <html lang={locale} suppressHydrationWarning>
+      <body
+        className={`${sendFlowers.variable} ${raleway.variable} ${notoSansThai.variable}`}
+      >
         <NextIntlClientProvider>
           <Provider>
             {children}
