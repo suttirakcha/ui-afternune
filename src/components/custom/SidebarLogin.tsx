d200879b2ee406e1b1e92cb1e1b41c5c5a72b4ca
suttirakcha <suttirak.char@gmail.com>
2026-06-13 17:27:57 +0700

    feat: set the notification length

--- src/components/custom/SidebarLogin.tsx
+++ src/components/custom/SidebarLogin.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import AvatarUser from "@/components/avatar/AvatarUser";
+import AfnBadge from "@/components/custom/AfnBadge";
 import AfnButton from "@/components/custom/AfnButton";
 import AfnMenu from "@/components/custom/AfnMenu";
 import { Option } from "@/types/menus.type";
 import { User } from "@/types/users.type";
+import { Stack } from "@chakra-ui/react";
 import { useTranslations } from "next-intl";
 import { useRouter } from "next/navigation";
 
@@ -22,5 +24,18 @@ export default function SidebarLogin({ profile, options }: SidebarLoginProps) {
     );
   }
 
-  return <AfnMenu trigger={<AvatarUser user={profile} />} options={options} />;
+  return (
+    <AfnMenu
+      trigger={
+        <Stack
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <AvatarUser user={profile} notification={2} />
+        </Stack>
+      }
+      options={options}
+    />
+  );
 }
