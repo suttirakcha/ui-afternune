ed768868030ccdb8a490195362adf1ce86d78150
suttirakcha <suttirak.char@gmail.com>
2026-08-24 11:18:40 +0700

    feat: adjust the registerSchema validation

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -14,6 +14,13 @@ export const registerSchema = Yup.object({
     .required("Email is a required field"),
   password: Yup.string()
     .min(8, "Password must be at least 8 characters")
+    .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .matches(/[a-z]/, "Password must contain at least one lowercase letter")
+    .matches(/[0-9]/, "Password must contain at least one number")
+    .matches(
+      /[@$!%*#?&]/,
+      "Password must contain at least one special character"
+    )
     .required("Password is a required field"),
   confirmPassword: Yup.string().required(
     "Confirm password is a required field"
